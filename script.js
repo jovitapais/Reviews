0f30c48481ee5ddea40aee45ad3dc1a84822433f
Jovita <jovitaspais@gmail.com>
2022-10-12 13:17:38 -0400

    click emojis to redirect to the pages as per the emotion

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const panel = document.querySelector("#panel");
-const sendButton = document.querySelector("#send");
-const ratingsContainer = document.querySelector(".ratings-container");
-const ratings = document.querySelectorAll(".rating");
+// const panel = document.querySelector("#panel");
+// const ratingsContainer = document.querySelector('.ratings-container');
+// const experience = document.querySelectorAll("small");
+const sendButton = document.querySelector('#send');
+const panelContainer = document.querySelector('.panel-container');
+const ratings = document.querySelectorAll('.rating');
+const experience = document.querySelectorAll('.active small');
 
 const removeActive = () => {
   for (let i = 0; i < ratings.length; i++) {
@@ -9,9 +12,24 @@ const removeActive = () => {
   }
 };
 
-ratingsContainer.addEventListener("click", (e) => {
-  if (e.target.parentNode.classList.contains("rating")) {
+panelContainer.addEventListener('click', (e) => {
+  if (e.target.parentNode.classList.contains('rating')) {
     removeActive();
-    e.target.parentNode.classList.add("active");
+    e.target.parentNode.classList.add('active');
   }
 });
+
+if (sendButton !== null) {
+  sendButton.addEventListener('click', () => {
+    const experience = document.querySelector('.active small');
+    console.log(experience);
+    if (
+      experience.innerHTML === 'Unhappy'
+			|| experience.innerHTML === 'Neutral'
+    ) {
+      window.location = 'negative_review.html';
+    } else {
+      window.location = 'positive_review.html';
+    }
+  });
+}
